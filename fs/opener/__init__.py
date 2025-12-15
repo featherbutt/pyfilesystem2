7d5cf597ac20d058332caf8c79180f9a251896ab
Featherbut <iamjohndoe220@gmail.com>
2025-12-14 17:51:27 -0800

    Remove deprecated namespace packages. This prevents us from using extensions, but we currently aren't anyway.

--- fs/opener/__init__.py
+++ fs/opener/__init__.py
@@ -2,9 +2,6 @@
 """Open filesystems from a URL.
 """
 
-# Declare fs.opener as a namespace package
-__import__("pkg_resources").declare_namespace(__name__)  # type: ignore
-
 # Import opener modules so that `registry.install` if called on each opener
 from . import appfs, ftpfs, memoryfs, osfs, tarfs, tempfs, zipfs
 
